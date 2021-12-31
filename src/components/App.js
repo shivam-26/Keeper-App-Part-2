416556a806905818264584e21010beb1928d6511
shivam-26 <79416882+shivam-26@users.noreply.github.com>
2021-12-31 18:48:09 +0530

    Keeper-App-Part-2

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((text) => (
+        <Note key={text.key} title={text.title} description={text.content} />
+      ))}
       <Footer />
     </div>
   );
